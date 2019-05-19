a9facfeaa5f4795d9c3352b366429f0af1be9f9a
Roland Huß <roland@ro14nd.de>
2019-05-19 17:56:55 +0200

    chore: Update health check

--- spring/src/main/java/io/k8spatterns/examples/HealthToggleIndicator.java
+++ spring/src/main/java/io/k8spatterns/examples/HealthToggleIndicator.java
@@ -5,7 +5,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-// Simple toggel to swith on/off the health check
+// Simple toggle to switch on/off the health check
 @Component
 public class HealthToggleIndicator implements HealthIndicator {
 
@@ -15,10 +15,26 @@ public class HealthToggleIndicator implements HealthIndicator {
     @Override
     public Health health() {
         Health.Builder builder = healthy ? Health.up() : Health.down();
-        return builder.withDetail("toggle", healthy).build();
+        Runtime r = Runtime.getRuntime();
+        return builder
+            .withDetail("toggle", healthy)
+            .withDetail("usedMemory", format(r.totalMemory() - r.freeMemory()))
+            .withDetail("totalMemory", format(r.totalMemory()))
+            .withDetail("maxMemory", format(r.maxMemory()))
+            .withDetail("freeMemory", format(r.freeMemory()))
+            .withDetail("availableProcessors", r.availableProcessors())
+            .build();
     }
 
     public void toggle() {
         this.healthy = !this.healthy;
+    }
+
+    public static String format(long v) {
+        if (v < 1024) return v + " B";
+        int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+        return String.format("%.1f %sB",
+                             (double)v / (1L << (z*10)),
+                             " KMGTPE".charAt(z));
     }
 }
